039c69eaf5ff077fe5eeb7ef40ae11aaf76c1483
QuickFixDev <quick.fix.dev1@gmail.com>
2023-10-29 23:25:26 -0600

    fixed server missing key

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("hello world");
+}); 
 
 app.get('/user-management', (req, res) => {
     console.log('Server route is triggered');
