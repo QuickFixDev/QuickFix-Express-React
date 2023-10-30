5960cbe47e3dae2ceb7b27d6f9f6839785e703c8
QuickFixDev <quick.fix.dev1@gmail.com>
2023-10-29 22:38:35 -0600

    added process port

--- server/server.js
+++ server/server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("hello world");
 });
 
 app.get('/user-management', (req, res) => {
