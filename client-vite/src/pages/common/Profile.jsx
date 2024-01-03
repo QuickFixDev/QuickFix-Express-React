794af7744a40c043c6ef63d31cd57e3d497efb2e
QuickFixDev <quickfix.dev1@gmail.com>
2024-01-03 00:54:45 -0600

    added custom heights and font sizes to rework profile page

--- client-vite/src/pages/common/Profile.jsx
+++ client-vite/src/pages/common/Profile.jsx
@@ -1,19 +1,41 @@
 import ProfileComponent from '../../components/common/ProfileComponent';
 import AccessDenied from '../../components/access/AccessDenied';
 import { useAuth } from "../../contexts/AuthContext";
+import { useAuth0 } from '@auth0/auth0-react';
+import { useResidences } from '../../hooks/useResidences';
 
 const Profile = () => {
-  const { authUser, isLoggedIn } = useAuth();
-  if (isLoggedIn) {
-    return (
-      <div className='container-fluid d-flex justify-content-center align-items-center'>
-        <ProfileComponent />
+  const { authUser } = useAuth()
+  const { user } = useAuth0()
+  const { residences, isLoading: residencesLoading } = useResidences({ tenant_id: authUser.Id })
+
+  const residence = residences[0]
+
+  return (
+    <>
+      <div className="container d-flex flex-row justify-content-center align-items-center h-90-vh">
+        <div className="row text-center row-cols-1">
+          <div className="col">
+            <img className='rounded-full' src={user.picture} alt="" style={{ width: '200px' }} />
+          </div>
+          <div className="col mt-4">
+            <h4 className='mt-1 fw-bold'>
+              {authUser.FirstName} {authUser.LastName}
+            </h4>
+          </div>
+          <div className="col mt-1 text-size-14">
+            {authUser.Email}
+          </div>  
+          {!residencesLoading && (
+            <div className="col mt-1 text-size-14">
+              {residence.street_name} No. {residence.street_number}
+            </div>
+          )}
+        </div>
       </div>
-    );
-  } else {
-    return <AccessDenied />
-  }
+    </>
+  )
 
-  };
+};
 
-  export default Profile;
+export default Profile;
